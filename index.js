2a1ea4f3697a524c68accf1611383cdbc032d455
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2019-03-04 16:56:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX
+ReactDOM.render(
+    <div>   
+        <ul>
+            <li>"1"</li>
+            <li>"2"</li>
+            <li>"3"</li>
+        </ul>
+    </div>, 
+document.getElementById("root"));
