54d08a1c9b57c8e256e9b3eec577ee81dc9d612c
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2019-03-06 22:17:23 -0500

    added a new css file to style the persons component.

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import './Person/Person.css'
+import { cursorTo } from 'readline';
 
 class App extends Component {
   state = {
@@ -23,6 +25,16 @@ switchNameHandler = ( ) => {
   })
 }
 
+nameChangedHandler = (event) => {
+  this.setState({
+    persons: [
+      {name: 'Abu', age: 27},
+      {name: 'Fasika', age: 25},
+      {name: event.target.value , age: 25}
+    ]
+  })
+}
+
 reverseNameHandler = () => {
   this.setState({
     persons: [
@@ -34,11 +46,22 @@ reverseNameHandler = () => {
 }
 
   render() {
+    // define a style variable that could be used inside the function.
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
           <h1>What up Daaaaaaaawwwwgggg!</h1>
-          <p>You know this shit going to be <strong>LITT! <Person /> </strong></p>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <p>You know this shit going to be <strong>LITT!</strong></p>
+          
+          <button 
+          style={style}
+          onClick={this.switchNameHandler}>Switch Name</button>
           {/* 'this' reffers to the class that we are in. */}
           <Person 
             name = {this.state.persons[0].name} 
@@ -51,7 +74,8 @@ reverseNameHandler = () => {
           </Person>
           <Person 
             name = {this.state.persons[2].name} 
-            age = {this.state.persons[2].age}>
+            age = {this.state.persons[2].age}
+            changed={this.nameChangedHandler}>
             Description: Fasika's nose!
           </Person>
       </div>
--- react-tutorial/src/Person/Person.js
+++ react-tutorial/src/Person/Person.js
@@ -3,10 +3,11 @@ import React from 'react';
 //ES6 syntax of functional component
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             {/* using props give access to all the attributes. */}
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
